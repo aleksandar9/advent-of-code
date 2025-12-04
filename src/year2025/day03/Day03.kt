212e387ae1f51edca84e9567e45dc4422f333b25
aleksandar9 <aleksandar.nine@gmail.com>
2025-12-04 01:17:12 +0100

    Day 03: Part 2 The math wasn't adding up for some reason so I decided to return a list of integers and later turn it to string and then to Long 😅

--- src/year2025/day03/Day03.kt
+++ src/year2025/day03/Day03.kt
@@ -5,6 +5,18 @@ import readInput
 
 fun main() {
 
+    fun calculateJoltage(batteryBank: List<Int>, num: Int): MutableList<Int> {
+        return if (num == 0) {
+            mutableListOf(batteryBank.max())
+        } else {
+            val max = batteryBank.dropLast(num).max()
+            val maxIndex = batteryBank.indexOf(max)
+            calculateJoltage(batteryBank.drop(maxIndex + 1), num - 1).apply {
+                add(max)
+            }
+        }
+    }
+
     fun part1(input: List<String>): Int {
         var sum = 0
 
@@ -19,8 +31,14 @@ fun main() {
         return sum
     }
 
-    fun part2(input: List<String>): Int {
-        var sum = 0
+    fun part2(input: List<String>): Long {
+        var sum = 0L
+
+        input.forEach {
+            val battery = it.map { it.digitToInt() }
+            val joltage = calculateJoltage(battery, 11)
+            sum += joltage.reversed().joinToString("").toLong()
+        }
 
         return sum
     }
@@ -28,7 +46,7 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput(pkg = "year2025/day03", name = "Day03_test")
     check(part1(testInput) == 357)
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 3121910778619L)
 
     val input = readInput(pkg = "year2025/day03", name = "Day03")
     part1(input).println()
